92fe3f8c60c3a0b9b54388284f9d4f5e074e8e97
jeremy302 <olajeremy123@gmail.com>
2023-04-05 18:52:39 +0100

    patch

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,2 +1,2 @@
 -- lists bands with glam rock stype
-SELECT band_name, (IFNULL(split, YEAR(NOW())) - formed) AS lifespan FROM metal_bands WHERE STYLE LIKE '%Glam rock%' ORDER BY lifespan DESC;
+SELECT band_name, (IFNULL(split, 2020) - formed) AS lifespan FROM metal_bands WHERE STYLE LIKE '%Glam rock%' ORDER BY lifespan DESC;
